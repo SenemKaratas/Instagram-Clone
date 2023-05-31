e62b88842c61beb3fc9807b7c13cb857e1061a96
Berkan B. Sosyal Medya  MacBook Pro <37027963+SenemKaratas@users.noreply.github.com>
2023-05-31 18:18:17 +0300

    Instagram-Clone
    clone project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container text-metal">
+      <p className="text-3xl font-bold underline">Hello world!</p>
+      <h1 className="text-xl">Deneme</h1>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
